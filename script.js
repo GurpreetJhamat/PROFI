2beb07209e7d889755fa95bb9e42f378a05b8ac8
GurpreetJhamat <gjhammt@gmail.com>
2022-04-21 18:59:40 -0400

    added testimonials section

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+import {testimonials} from "./testimonials.js"
 const slider = document.querySelector(".slider-img");
 const prevBtn = document.querySelector('.btn-prev')
 const nextBtn = document.querySelector('.btn-next')
 const imgs = document.querySelectorAll(".slider-img .content");
 // console.log(img)
+const toggleBtn = document.querySelector(".toggle");
+const navCollapse = document.querySelector(".nav-collapse");
 
+const panels = document.querySelectorAll(".img");
+
+const labels = document.querySelectorAll(".contact-form label");
+
+const testimonialsContainer = document.querySelector(".testimonials-container");
+const testimonial = document.querySelector(".testimonial");
+const userImage = document.querySelector(".user-image");
+const username = document.querySelector(".username");
+const role = document.querySelector(".role");
 let idx = 0;
 
 function changeImg(){
@@ -26,9 +38,8 @@ prevBtn.addEventListener('click', () => {
     console.log(idx)
 })
 
-const toggleBtn = document.querySelector('.toggle');
-const navCollapse = document.querySelector('.nav-collapse');
-// nav - collapse;
+
+
 
 toggleBtn.addEventListener('click', () => {
     navCollapse.classList.toggle("show-collapse");
@@ -36,7 +47,7 @@ toggleBtn.addEventListener('click', () => {
 })
 
 
-const panels = document.querySelectorAll(".img");
+
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
@@ -49,3 +60,40 @@ function removeActiveClass() {
     panel.classList.remove("active");
   });
 }
+
+
+
+labels.forEach((label) => {
+    console.log(label.innerHTML);
+    label.innerHTML = label.innerText
+    .split("")
+    .map((letter, idx) => {
+        // console.log(letter)
+        return `<span style="transition-delay:${
+          idx * 50
+        }ms"><b>${letter}</b></span>`;
+    })
+    .join("")
+
+    console.log(label.innerHTML)
+})
+
+
+let index = 1;
+
+function updateTestimonial() {
+  const { name, position, photo, text } = testimonials[index];
+
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+  role.innerHTML = position;
+
+  index++;
+
+  if (index > testimonials.length - 1) {
+    index = 0;
+  }
+}
+
+setInterval(updateTestimonial, 10000);
